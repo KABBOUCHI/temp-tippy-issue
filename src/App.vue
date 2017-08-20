874b85e84adb26dc542ad704ae6583aad5a3577e
iamdubx <code@dubx.me>
2017-08-20 13:27:32 -0400

    google maps issue

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view></router-view>
+    <button class="btn"
+            v-tippy
+            data-distance="15"
+            data-theme="light"
+            data-animation="scale"
+            data-arrowsize="big"
+            data-interactive="true"
+            data-animatefill="false"
+            data-arrow="true"
+            data-trigger="click"
+            data-html="#comppopup">
+                Popover Vue Component
+    </button>
+    <GoogleMaps style="display: none" id="comppopup"></GoogleMaps>
+  </div>
+</template>
+
+<script>
+import GoogleMaps from './components/GoogleMaps.vue'
+
+export default {
+  name: 'app',
+  components: {
+    GoogleMaps
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/GoogleMaps.vue
@@ -0,0 +1,200 @@
+<template>
+<div class="temp">
+  <gmap-map
+    :options="{styles: styles}"
+    :center="{lat:10, lng:10}"
+    :zoom="7"
+    map-type-id="terrain"
+    style="display: block; width: auto; height: 300px"
+  ></gmap-map>
+</div>
+</template>
+
+<script>
+const API_KEY = 'AIzaSyDTHFHSlstw8dCYPOj3S4QXYhB3HSkZny8'
+import Vue from 'vue'
+import * as VueGoogleMaps from 'vue2-google-maps'
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: API_KEY
+    // libraries: 'places',
+    // This is required if you use the Autocomplete plugin
+    // OR: libraries: 'places,drawing'
+    // OR: libraries: 'places,drawing,visualization'
+    // (as you require)
+  }
+})
+export default {
+  data () {
+    return {
+      styles: [
+        {
+          'elementType': 'geometry',
+          'stylers': [
+            {
+              'color': '#f5f5f5'
+            }
+          ]
+        },
+        {
+          'elementType': 'labels.icon',
+          'stylers': [
+            {
+              'visibility': 'off'
+            }
+          ]
+        },
+        {
+          'elementType': 'labels.text.fill',
+          'stylers': [
+            {
+              'color': '#616161'
+            }
+          ]
+        },
+        {
+          'elementType': 'labels.text.stroke',
+          'stylers': [
+            {
+              'color': '#f5f5f5'
+            }
+          ]
+        },
+        {
+          'featureType': 'administrative.land_parcel',
+          'elementType': 'labels.text.fill',
+          'stylers': [
+            {
+              'color': '#bdbdbd'
+            }
+          ]
+        },
+        {
+          'featureType': 'poi',
+          'elementType': 'geometry',
+          'stylers': [
+            {
+              'color': '#eeeeee'
+            }
+          ]
+        },
+        {
+          'featureType': 'poi',
+          'elementType': 'labels.text.fill',
+          'stylers': [
+            {
+              'color': '#757575'
+            }
+          ]
+        },
+        {
+          'featureType': 'poi.park',
+          'elementType': 'geometry',
+          'stylers': [
+            {
+              'color': '#e5e5e5'
+            }
+          ]
+        },
+        {
+          'featureType': 'poi.park',
+          'elementType': 'labels.text.fill',
+          'stylers': [
+            {
+              'color': '#9e9e9e'
+            }
+          ]
+        },
+        {
+          'featureType': 'road',
+          'elementType': 'geometry',
+          'stylers': [
+            {
+              'color': '#ffffff'
+            }
+          ]
+        },
+        {
+          'featureType': 'road.arterial',
+          'elementType': 'labels.text.fill',
+          'stylers': [
+            {
+              'color': '#757575'
+            }
+          ]
+        },
+        {
+          'featureType': 'road.highway',
+          'elementType': 'geometry',
+          'stylers': [
+            {
+              'color': '#dadada'
+            }
+          ]
+        },
+        {
+          'featureType': 'road.highway',
+          'elementType': 'labels.text.fill',
+          'stylers': [
+            {
+              'color': '#616161'
+            }
+          ]
+        },
+        {
+          'featureType': 'road.local',
+          'elementType': 'labels.text.fill',
+          'stylers': [
+            {
+              'color': '#9e9e9e'
+            }
+          ]
+        },
+        {
+          'featureType': 'transit.line',
+          'elementType': 'geometry',
+          'stylers': [
+            {
+              'color': '#e5e5e5'
+            }
+          ]
+        },
+        {
+          'featureType': 'transit.station',
+          'elementType': 'geometry',
+          'stylers': [
+            {
+              'color': '#eeeeee'
+            }
+          ]
+        },
+        {
+          'featureType': 'water',
+          'elementType': 'geometry',
+          'stylers': [
+            {
+              'color': '#c9c9c9'
+            }
+          ]
+        },
+        {
+          'featureType': 'water',
+          'elementType': 'labels.text.fill',
+          'stylers': [
+            {
+              'color': '#9e9e9e'
+            }
+          ]
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+.temp {
+  width: 300px;
+}
+</style>
